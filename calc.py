e78c69a04c71c1073461504aaa8b8409dfbd0b43
We-ndytp <wposada1@babson.edu>
2016-09-03 00:19:23 -0400

    calc.py, E2Q_All

--- calc.py
+++ calc.py
@@ -3,4 +3,4 @@ print(10/1.60934) #question 2
 #question 3
 print(2565/6.21371) #avg time in sec/m
 print(412.7968637094425/60) #avg time in min/m
-print(60/6.879947728490708) #mph 
+print(60/6.879947728490708) #mph
