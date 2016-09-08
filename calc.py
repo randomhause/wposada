87ae663110a51c45ffd8a092b460b1aa0ad5cb11
We-ndytp <wposada1@babson.edu>
2016-09-07 22:45:37 -0400

    Class 2_Excercise 1

--- calc.py
+++ calc.py
@@ -7,44 +7,34 @@
 
 #Class 2_Exercise 1
 #problem1
-#message = 'Enter the Radius of the Sphere: '
-#print(message)
-#radius = float(input())
-#pi = 3.14
-#volume = (4/3) * (pi * radius ** 3)
-#print('Volume is: ', volume)
+message = 'Enter the Radius of the Sphere: '
+print(message)
+radius = float(input())
+pi = 3.14
+volume = (4/3) * (pi * radius ** 3)
+print('Volume is: ', volume)
 
 #problem2
-#price = 24.95
-#discount = price * .4
-#quantity = 60
-#shipping = 3 + ((quantity-1) * .75)
-#wholesale = (price - discount) * quantity + shipping 
-#message = 'Total wholesale cost for 60 copies is ', wholesale
-#print(message)
+price = 24.95
+discount = price * .4
+quantity = 60
+shipping = 3 + ((quantity-1) * .75)
+wholesale = (price - discount) * quantity + shipping 
+message = 'Total wholesale cost for 60 copies is:', wholesale
+print(message)
  
 #problem3
-seconds = 1
-minutes = 60 * seconds
-hours = 60 * minutes
+easy_time = 2 * 8.25
+tempo_time = 3 * 7.2
+total = easy_time + tempo_time
+end_minutes = total - 8 #the amount of min past 7am
+message = 'Home for breakfast: 7:%d' % end_minutes
+print(message)
 
-time_left_house = 6 * hours + 52 * minutes
-miles_run_easy_pace = 2 * (8 * minutes + 15 * seconds)
-miles_run_fast_pace = 3 * (7 *minutes + 12 * seconds)
-#output in seconds
+#problem4
+increase = 89 - 82
+inc = increase / 82
+PercInc = inc * 100
+message = 'Percent Increase: %.1f%%' % PercInc
+print(message)
 
-total_time_run = miles_run_easy_pace + miles_run_fast_pace + time_left_house
-
-hours2 = total_time_run // hours
-#hour output
-
-part_hour = total_time_run % hours
-minutes2 = part_hour // minutes
-seconds2 = part_hour % minutes
-
-print('Total time run: {}, Hours: {}, Minutes: {}, Seconds: {}'.format(total_time_run, hours2, minutes2, seconds2))
-
-
-
-
-#proble4
