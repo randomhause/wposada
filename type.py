ae0763d0eafb027258a670f4c1c38cfa750c33ac
We-ndytp <wposada1@babson.edu>
2016-09-13 13:32:08 -0400

    type

---
+++ type.py
@@ -0,0 +1,12 @@
+#Class03_Excercise
+
+#problem 3
+import time
+current_time = int(1437746094.5735958)
+minutes = current_time // 60 #// to take away reminders
+hours = minutes // 60
+num_day = hours // 60
+print(num_day, 'days since epoch')
+
+
+
