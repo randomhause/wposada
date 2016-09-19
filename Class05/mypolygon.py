a3112b726b25e3afaf387671fac8f48e248b5c32
We-ndytp <wposada1@babson.edu>
2016-09-18 22:07:00 -0400

    HW: Class05_shape1

--- Class05/mypolygon.py
+++ Class05/mypolygon.py
@@ -1,7 +1,40 @@
 import turtle
 jerry = turtle.Turtle()
+jerry.pensize(2)
+import math
 
-'''Excersie 2.1
+
+#Excercise 3_triangle
+print(jerry_1)
+jerry_1.fd = 200
+
+turtle.mainloop()
+
+
+#Excercise 3_flower
+'''print(jerry)
+
+def polyline(t, n, lenght, angle):
+    for i in range(n):
+        t.fd(lenght)
+        t.lt(angle)
+
+def polygon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ Excersie 2.1
 def square(t):
     for i in range(4):
         t.fd(100)
@@ -21,13 +54,14 @@ def polygon(t, n, length):
 polygon(jerry, 7, 70)
 
 '''
-'''Excercise 2.4'''
+'''Excercise 2.4
 def circle(t, r):
     circumference = 2 * math.pi * r
     n = 50
     length = circumference / n
    
    polygon(t, n, length)
+'''
 
 
-turtle.mainloop()
+
---
+++ Class05/shape-exercise_1.py
@@ -0,0 +1,59 @@
+import turtle
+import math
+tpen = turtle.Turtle()
+tpen.pensize(4)
+tpen.hideturtle()
+
+print(tpen)
+
+#shape one
+def twotris(t):
+    t.lt(30)
+    t.fd(200)
+    t.rt(120)
+    t.fd(200)
+    t.rt(120)
+    t.fd(400)
+    t.lt(120)
+    t.fd(200)
+    t.lt(120)
+    t.fd(200)
+
+twotris(tpen)
+
+tpen.lt(60)
+twotris(tpen)
+
+tpen.fd(200)
+tpen.lt(91)
+tpen.circle(200)
+tpen.lt(90)
+tpen.fd(100)
+tpen.circle(55)
+
+tpen.fd(100)
+tpen.lt(30)
+
+tpen.fd(100)
+tpen.circle(55)
+
+tpen.lt(180)
+tpen.fd(100)
+
+tpen.rt(270)
+tpen.fd(100)
+tpen.circle(55)
+
+tpen.lt(180)
+tpen.fd(100)
+
+tpen.rt(270)
+tpen.fd(100)
+tpen.circle(55)
+
+
+
+
+
+
+turtle.mainloop()
