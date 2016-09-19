3c77c35a709e01e95bb06843b76a377868d91fda
We-ndytp <wposada1@babson.edu>
2016-09-18 20:33:52 -0400

    commits

--- Class05/mypolygon.py
+++ Class05/mypolygon.py
@@ -1,4 +1,33 @@
 import turtle
 jerry = turtle.Turtle()
 
-print(jerry)
+'''Excersie 2.1
+def square(t):
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+
+square(jerry)
+
+'''
+'''Excercise 2.3
+
+def polygon(t, n, length):
+    angle = 360 / n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+polygon(jerry, 7, 70)
+
+'''
+'''Excercise 2.4'''
+def circle(t, r):
+    circumference = 2 * math.pi * r
+    n = 50
+    length = circumference / n
+   
+   polygon(t, n, length)
+
+
+turtle.mainloop()
--- calc.py
+++ calc.py
@@ -41,22 +41,20 @@
 
 #Session04_Excercise_1_9/13
 
-'''
-def quadratic_equation(a, b, c):
-    ...
-'''
+from math import sqrt
 
-import cmath #call module 
-a = float(input('Enter a: '))
-b = float(input('Enter b: '))
-c = float(input('Enter c: '))
-d = (b**2) - (4*a*c) #discriminant
-
-#find Positive & Negative 
-pos = (-b+ cmath.sqrt(d))/(2*a)
-neg = (-b- cmath.sqrt(d))/(2*a)
-
-print('Solving for the quadratic equation: {0} and {1}'.format(pos,neg))
+def quadratic(a, b, c):
+    a = float(input('Enter a: '))
+    b = float(input('Enter b: '))
+    c = float(input('Enter c: '))
+    
+    d = (b**2) - (4*a*c) #discriminant
+    
+    try:
+        pos = (-b + (sqrt(d)) / (2*a)
+        neg = (-b - (sqrt(d)) / (2*a)
+        return pos, neg
+ 
 
 #Worked with a group.
 
