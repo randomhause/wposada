3d5a790cf8e12759d6ec13906c445f1238976acc
We-ndytp <wposada1@babson.edu>
2016-09-27 13:34:48 -0400

    Session 8: Quiz_1

---
+++ Quiz/quiz1.py
@@ -0,0 +1,20 @@
+#Question1
+"""
+    a, b: two integers
+    Returns True if either one is 6, or if their sum or difference is 6. 
+    """
+def crazy_about_9(a, b): 
+    a = int()
+    b = int()
+if a == 9:
+    print(True)
+if b == 9:
+    print(true)
+if a + b == 9:
+    print(True)
+if a - b == 9:
+    print(True)
+
+print(crazy_about_9(2, 9))
+print(crazy_about_9(4, 5))
+print(crazy_about_9(3, 8))
