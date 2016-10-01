820fa64d445a8a6bb8ae545aec317f2d8eb7561d
We-ndytp <wposada1@babson.edu>
2016-09-30 21:20:14 -0400

    Session 09 HW

---
+++ Class09/class_excercise.py
@@ -0,0 +1,9 @@
+#Session09
+#Exercise02
+def is_abecedarian(word):
+    if len(word) <= 1:
+        return True
+    if word[0] > word[1]:
+        return False
+    return is_abecedarian(word[1:])
+
