28504effca00760d5b413ad18fd25727570b52f8
We-ndytp <wposada1@babson.edu>
2016-09-30 21:03:05 -0400

    HW_S09_Ex3

---
+++ Class09/word.py
@@ -0,0 +1,13 @@
+fin = open('words.txt')
+line = repr(fin.readline())
+# https://docs.python.org/3/library/functions.html#repr
+
+def read_long_words():
+    wordList = fd.read() .split()
+    if len(word) > 20 
+
+print(wordList)
+
+
+
+
