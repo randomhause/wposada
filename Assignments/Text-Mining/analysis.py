785785f89e636a7d78d8fe7b3943408c3a249788
We-ndytp <wposada1@babson.edu>
2016-10-30 22:37:02 -0400

    Lyrics

--- Assignments/Text-Mining/analysis.py
+++ Assignments/Text-Mining/analysis.py
@@ -1,19 +1,136 @@
-from Tokenization import *
-import nltk
+import random
+import string
 
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+def process_file(filename, skip_header):
+    """Makes a histogram that contains the words from a file.
 
-def create_text(hist):
-    '''
-    Convering dict to str on word frequencies.
-    Keys = str words
-    Value = Int word freq
-    '''
+    filename: string
+    skip_header: boolean, whether to skip the Gutenberg header
 
-    result = str()
-    for i in hist:
-        for j in range(hist[i]):
-            result = result + ' ' + i
-    return result
+    returns: map from each word to the number of times it appears.
+    """    
+    hist = {}    
+    fp = open(filename, encoding ='utf8')
 
-def cosine_sim()
+    #if skip_header:
+        #skip_gutenber_header(fp)
+        
+    for line in fp:
+        if line.startswith('*** END OF THIS PROJECT'):
+            break
+        line = line.replace('-', ' ')
+        strippables = string.punctuation + string.whitespace
+        
+        # INSERT CODE BELOW
+        for word in line.split():  #line.split() splits words
+            word = word.lower()
+        
+        # update the histogram
+        hist[word] = hist.get(word, 0) + 1 
+        
+
+    return hist
+#print(process_file('hip_pop.txt'))
+
+def skip_gutenberg_header(fp):
+    """Reads from fp until it finds the line that ends the header.
+
+    fp: open file object
+    """
+    for line in fp:
+        if line.startswith('*** START OF THIS PROJECT'):
+            break
+
+
+def total_words(hist):
+    """Returns the total of the frequencies in a histogram."""
+    return sum(hist.values())
+    
+#hist = process_file('text_file.txt', skip_header=False)
+#print(total_words(hist))
+
+def different_words(hist):
+    """Returns the number of different words in a histogram."""
+    return len(hist)
+
+
+def most_common(hist):
+    """Makes a list of word-freq pairs in descending order of frequency.
+
+    hist: map from word to frequency
+
+    returns: list of (frequency, word) pairs
+    
+    temp = [] #is a list
+    for word, frequency in hist.item(): #words and frequency - but and only works for logical (true or false)
+        temp.append((frequency, word)) #list of trouples
+        #trouples list immudable, return multiple variables 
+        """
+    t = []
+    for key, value in hist.items():
+        t.append((value, key))
+
+    t.sort()
+    t.reverse()
+    return t
+
+def print_most_common(hist, num=10):
+    """Prints the most commons words in a histgram and their frequencies.
+
+    hist: histogram (map from word to frequency)
+    num: number of words to print
+    """
+    t = most_common(hist)
+    print('The most common words are:')
+    for freq, word in t[:num]:
+        print(word, '\t', freq)
+
+
+def subtract(d1, d2):
+    """Returns a dictionary with all keys that appear in d1 but not d2.
+
+    d1, d2: dictionaries
+    """
+    res = {}
+    for key in d1:
+        if key not in d2:
+            res[key] = None
+    return res
+
+
+def random_word(hist):
+    """Chooses a random word from a histogram.
+
+    The probability of each word is proportional to its frequency.
+    """
+    t = []
+    for word, freq in hist.items():
+        t.extend([word] * freq)
+
+    return random.choice(t)
+
+
+def main():
+    hist = process_file('hip_pop.txt', skip_header=True)
+    print('Total number of words:', total_words(hist))
+    print('Number of different words:', different_words(hist))
+
+    t = most_common(hist)
+    print('The most common words are:')
+    for freq, word in t[0:20]:
+        print(word, '\t', freq)
+
+    words = process_file('words.txt', skip_header=False)
+
+    diff = subtract(hist, words)
+    print("The words in the genre that aren't in the word list are:")
+    for word in diff.keys():
+        print(word, end=' ')
+
+    print("\n\nHere are some random words from the genre")
+    for i in range(100):
+        print(random_word(hist), end=' ')
+
+
+if __name__ == '__main__':
+    main()
--- Assignments/Text-Mining/lyrics.py
+++ Assignments/Text-Mining/lyrics.py
@@ -8,41 +8,35 @@ hip_pop.append(PyLyrics.getLyrics('Afrika Bambaataa','Planet Rock'))
 #hip_pop.append(PyLyrics.getLyrics('Run-D.M.C.','Sucker M.C.s')) #example of a song that did not show up
 hip_pop.append(PyLyrics.getLyrics('Geto Boys','Mind Playing Tricks on Me'))
 hip_pop.append(PyLyrics.getLyrics('Snoop Dogg','Nuthin But a G Thang'))
-hip_pop.append(PyLyrics.getLyrics('Public Enemy','Fight the Power'))
-hip_pop.append(PyLyrics.getLyrics('Notorious B.I.G.','Juicy'))
-hip_pop.append(PyLyrics.getLyrics('Eric B.','Paid in Full'))
 
+#Unable to create file from the following
 country =[PyLyrics.getLyrics('Johnny Cash', 'I Walk the Line')]
 country.append(PyLyrics.getLyrics('Patsy Cline', 'Crazy'))
 country.append(PyLyrics.getLyrics('Gretchen Wilson','Redneck Woman'))
 country.append(PyLyrics.getLyrics('George Jones', 'He Stopped Loving Her Today'))
-country.append(PyLyrics.getLyrics('Jimmie Rodgers', 'Blue Yodel 9'))
-country.append(PyLyrics.getLyrics('Tammy Wynette', 'Stand By Your Man'))
-country.append(PyLyrics.getLyrics('Ray Charles', 'You Dont Know Me'))
-country.append(PyLyrics.getLyrics('Merle Haggard', 'Mama Tried'))
-country.append(PyLyrics.getLyrics('Dolly Parton', 'Jolene'))
-country.append(PyLyrics.getLyrics('Waylon Jennings and Willie Nelson', 'Mammas, Dont Let Your Babies Grow Up to Be Cowboys'))
+country.append(PyLyrics.getLyrics('Gary Allan', 'Songs about rain'))
+
 
 motown =[PyLyrics.getLyrics('The Temptations', 'My Girl')]
 motown.append(PyLyrics.getLyrics('Marvin Gaye', 'Whats Going On'))
 motown.append(PyLyrics.getLyrics('Marvin Gaye', 'I Heard it Through the Grapevine'))
-motown.append(PyLyrics.getLyrics('The Temptations', 'Papa was a Rolling Stone'))
-motown.append(PyLyrics.getLyrics('Smokey Robinson and the Miracles', 'Tracks of My Tears'))
+#motown.append(PyLyrics.getLyrics('The Temptations', 'Papa was a Rolling Stone'))
+#motown.append(PyLyrics.getLyrics('Smokey Robinson and the Miracles', 'Tracks of My Tears'))
 motown.append(PyLyrics.getLyrics('The Temptations', 'Aint Too Proud to Beg'))
-motown.append(PyLyrics.getLyrics('Smokey Robinson and the Miracles', 'Tears of a Clown'))
+#motown.append(PyLyrics.getLyrics('Smokey Robinson and the Miracles', 'Tears of a Clown'))
 motown.append(PyLyrics.getLyrics('Marvin Gaye and Tammi Terrell', 'Aint No Mountain High Enough'))
 motown.append(PyLyrics.getLyrics('Martha and the Vandellas', 'Dancing in the Street'))
-motown.append(PyLyrics.getLyrics('Marvin Gaye', 'Lets Get it On'))
+#motown.append(PyLyrics.getLyrics('Marvin Gaye', 'Lets Get it On'))
 
 boyband = [PyLyrics.getLyrics('The Jackson 5', 'I Want You Back')]
-boyband.append(PyLyrics.getLyrics('The Jackson 5', 'Ill be There'))
+#boyband.append(PyLyrics.getLyrics('The Jackson 5', 'Ill be There'))
 boyband.append(PyLyrics.getLyrics('Bay City Rollers', 'Saturday Night'))
-boyband.append(PyLyrics.getLyrics('The Monkees', 'Im a Believer'))
+#boyband.append(PyLyrics.getLyrics('The Monkees', 'Im a Believer'))
 boyband.append(PyLyrics.getLyrics('Backstreet Boys', 'I Want It That Way'))
-boyband.append(PyLyrics.getLyrics('N Sync', 'Bye, Bye, Bye'))
+#boyband.append(PyLyrics.getLyrics('N Sync', 'Bye, Bye, Bye'))
 boyband.append(PyLyrics.getLyrics('The Jackson 5', 'ABC'))
 boyband.append(PyLyrics.getLyrics('Take That', 'Back for Good'))
-boyband.append(PyLyrics.getLyrics('New Kids on the Block', 'You Got It'))
+#boyband.append(PyLyrics.getLyrics('New Kids on the Block', 'You Got It'))
 boyband.append(PyLyrics.getLyrics('One Direction', 'Story of My Life'))
 
 randb = [PyLyrics.getLyrics('Otis Redding', 'I Cant Turn You Loose')]
@@ -54,19 +48,21 @@ randb.append(PyLyrics.getLyrics('Stevie Wonder', 'Uptight'))
 randb.append(PyLyrics.getLyrics('James Brown', 'Please, Please, Please'))
 randb.append(PyLyrics.getLyrics('Smokey Robinson', 'Shop Around'))
 randb.append(PyLyrics.getLyrics('Luther Vandross', 'Here and Now'))
-randb.append(PyLyrics.getLyrics('B.B King', 'You Upset Me Baby'))
+#randb.append(PyLyrics.getLyrics('B.B King', 'You Upset Me Baby'))
 
 
 
 
-f = open("hip_pop.py", "w")
+f = open("hip_pop.txt", "w")
 g_hp = [hip_pop]
 f.write("\n".join(map(lambda x: str(x), g_hp)))
 f.close()
 
+#Unable to Open to other files
+'''
 f2 = open("country.py", "w")
 g_c = [country]
-f.write("\n".join(map(lambda x: str(x), g_c)))
+f.write("\n".join(map(lambda y: str(y), g_c)))
 f.close()
 
 f3 = open("motown.py", "w")
@@ -86,7 +82,7 @@ f.close()
 
 
 #Test the following print(genre) list 
-'''
+
 print(hip_pop[0])
 print('-------------------------------------------------------------------------')
 print('-------------------------------------------------------------------------')
@@ -100,7 +96,7 @@ print('-------------------------------------------------------------------------
 print('-------------------------------------------------------------------------')
 print('-------------------------------------------------------------------------')
 #pickup 
-'''
+
 
 #pop = 
 
@@ -109,5 +105,4 @@ print('-------------------------------------------------------------------------
 
 
 #pickup 
-
 '''
